a52ffd9b4cc4c40a09d0bb2c7f17e880db6ab398
yabessy <johanliebert.outergod@gmail.com>
2022-12-25 17:45:48 +0700

    create api login response

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -16,12 +16,14 @@ const userSchema = new Schema({
 })
 
 userSchema.statics.signup = async function (email, password) {
-  if (!email || !password) {
-    throw Error("Email and password are required")
-  } else if (!isEmail(email)) {
-    throw Error("Email is not valid")
-  } else if (
-    !isStrongPassword(password, { minLength: 8, minUppercase: 1, minNumbers: 1 })
+  if (!email || !password) throw Error("Email and password are required")
+  if (!isEmail(email)) throw Error("Email is not valid")
+  if (
+    !isStrongPassword(password, {
+      minLength: 8,
+      minUppercase: 1,
+      minNumbers: 1,
+    })
   ) {
     throw Error("Password is not strong enough")
   }
@@ -33,6 +35,17 @@ userSchema.statics.signup = async function (email, password) {
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
   const user = await this.create({ email, password: hash })
+  return user
+}
+
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("Email and password are required")
+  }
+  const user = await this.findOne({ email })
+  if (!user) throw Error("Incorect Email")
+  const match = await bcrypt.compare(password, user.password)
+  if(!match) throw Error('Incorect password')
   return user
 }
 
