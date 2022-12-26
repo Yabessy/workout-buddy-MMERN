0448f97776edb82c3a8bfa15a2ef0d742aa03229
yabessy <johanliebert.outergod@gmail.com>
2022-12-26 20:29:01 +0700

    protect api routes

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -6,8 +6,10 @@ const {
   deleteWorkout,
   updateWorkout,
 } = require("../controllers/workoutController")
+const requireAuth = require("../middleware/requireAuth")
 
 const router = express.Router()
+router.use(requireAuth)
 
 router.get("/", getAllWorkouts)
 router.get("/:id", getSingleWorkout)
